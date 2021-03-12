eda77b587cb16d71c5e9206fa3c15d0b12b64239
pete3249 <katelynrpeterson16@gmail.com>
2021-03-12 09:11:21 -0500

    Add devise controllers and routes

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :created_events, class_name: 'Event', foreign_key: 'event_creator_id'
+  has_many :invited_events, class_name: 'Event', foreign_key: 'invited_user_id'
+  has_many :user_events, foreign_key: 'invited_user_id'
+  has_many :events, through: :user_events
 end
