e86d4b0f5ea00aeaeafc8f45e66f53fb87459699
pete3249 <katelynrpeterson16@gmail.com>
2021-03-12 20:28:04 -0500

    Add additional relationships

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,6 @@
 class Event < ApplicationRecord
   belongs_to :event_creator, class_name: "User"
+  has_many :user_events
+  has_many :attendees,->{going: true}, through: :user_events, source: :invited_user
+  has_many :invited_users,->{going: false}, through: :user_events, source: :invited_user
 end
