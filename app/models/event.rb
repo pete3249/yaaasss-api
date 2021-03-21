fef406592ab50132e2cae55d3fd5a33ac28c3731
pete3249 <katelynrpeterson16@gmail.com>
2021-03-20 21:23:15 -0400

    Fix association for has_many through

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,7 @@
 class Event < ApplicationRecord
   belongs_to :event_creator, class_name: "User"
   has_many :user_events
-  has_many :attendees, -> { where('going: true')}, through: :user_events, source: :invited_user
-  has_many :invited_users, -> { where('going: false')}, through: :user_events, source: :invited_user
+  has_many :invited_users, through: :user_events, source: :invited_user
 end
+
+
