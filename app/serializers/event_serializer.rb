08f874d8c964dd5622f8d1a060b7628b10da03e9
pete3249 <katelynrpeterson16@gmail.com>
2021-03-21 11:21:03 -0400

    Add scope method for users

--- app/serializers/event_serializer.rb
+++ app/serializers/event_serializer.rb
@@ -1,4 +1,17 @@
 class EventSerializer
   include JSONAPI::Serializer
-  attributes :id, :name, :start_time, :end_time, :location, :address, :notes
+  attributes :id, :name, :start_time, :end_time, :location, :address, :notes, :invited_users, :attendees
+
+  attribute :invited_users do |events|
+    events.invited_users.map do |user|
+      { id: user.id, username: user.username}
+    end
+  end 
+
+  attribute :attendees do |events|
+    events.attendees.map do |user|
+      { id: user.id, username: user.username}
+    end
+  end 
+
 end
