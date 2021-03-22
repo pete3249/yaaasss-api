1265ae2a0d381abeb4d4c83c8a737b1b9b46f71e
pete3249 <katelynrpeterson16@gmail.com>
2021-03-22 11:37:42 -0400

    Add dependent for delete action

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,8 @@
 class Event < ApplicationRecord
   belongs_to :event_creator, class_name: "User"
-  has_many :user_events
+  has_many :user_events, :dependent => :destroy
   has_many :invited_users, -> { where 'going = false'}, through: :user_events, source: :invited_user
   has_many :attendees, -> { where 'going = true'}, through: :user_events, source: :invited_user
+
+  validates :name, presence: true
 end
