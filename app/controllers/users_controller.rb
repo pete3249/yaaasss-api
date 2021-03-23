6a02ef63f0454a6670be08765f942270600684ae
pete3249 <katelynrpeterson16@gmail.com>
2021-03-23 01:05:38 -0400

    Include currentUser in JSON

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,9 @@ class UsersController < ApplicationController
   def index
     @users = User.all
 
-    render json: UserSerializer.new(@users).serializable_hash[:data].map{|user| user[:attributes]}.filter{|user| user[:id] != 1}
+    render json: { 
+      users: UserSerializer.new(@users).serializable_hash[:data].map{|user| user[:attributes]}.filter{|user| user[:id] != 1},
+      current_user: current_user.id
+    }
   end
 end
